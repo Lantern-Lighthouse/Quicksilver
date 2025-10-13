e0ce19f14755e4336b269b1fecea8355c60b63a5
Písek Pískovec <pisekpiskovec@gmail.com>
2025-10-14 01:42:28 +0200

    feat: Search page

--- app/Controllers/Index.php
+++ app/Controllers/Index.php
@@ -7,5 +7,16 @@ class Index {
         $base->set("content", "home.html");
         echo \Template::instance()->render("index.html");
     }
+
+    public function getSearch(\Base $base): void {
+        $query = $base->get("GET.q");
+        $data = file_get_contents($base->get("QS.ATHEJA_SERVER_URL") . "/api/search?q=" . $query);
+        $result = json_decode($data, true);
+        $resultCount = $result["total_results"];
+        $base->set("entries_count", $resultCount);
+        $base->set("entries", $result["results"]);
+        $base->set("content", "search.html");
+        echo \Template::instance()->render("index.html");
+    }
 }
 
