34d34212f26e6098647130ac432a75beb2b8b6ac
Písek Pískovec <pisekpiskovec@gmail.com>
2025-10-25 21:30:50 +0200

    feat: Sanitize server url

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ require('./vendor/autoload.php');
 $base = \Base::instance();
 $base->config('./app/Configs/config.ini');
 
+if(str_ends_with($base->get("QS.ATHEJA_SERVER_URL"), '/')) {
+    $base->set("QS.ATHEJA_SERVER_URL", substr($base->get("QS.ATHEJA_SERVER_URL"), 0, strlen($base->get("QS.ATHEJA_SERVER_URL")) - 1));
+}
+
 if($base->get("SESSION.token_expire") < date('Y-m-d H:i:s')) {
     $base->clear("SESSION");
 }
