385ae1be2df3a3b64aedb044048bd3635c4dc9ba
Písek Pískovec <pisekpiskovec@gmail.com>
2025-11-02 20:09:43 +0100

    feat: add entry editing functionality
    - Add edit entry routes for GET and POST /edit/entry/@entryID endpoints to Post controller
    - Add entry editing functionality with GET handler to display edit form and POST handler to submit changes
    - Create entry edit form view with pre-filled name and description fields and read-only URL display
    - Add upvote/downvote placeholders and edit link to search results entry display

--- app/Controllers/Post.php
+++ app/Controllers/Post.php
@@ -12,7 +12,7 @@ class Post {
         }
 	}
 
-    public function postEnter(\Base $base): void {
+    public function postEnter(\Base $base) {
 		if($base->get("SESSION.token") === null) {
         	$base->reroute("/error");
         }
@@ -30,6 +30,64 @@ class Post {
 
         $options = [
             CURLOPT_URL => $base->get("QS.ATHEJA_SERVER_URL") . "/api/search/entry/create",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => http_build_query($postFields),
+            CURLOPT_HTTPHEADER => array("Authorization: Bearer " . $base->get("SESSION.token")),
+        ];
+
+        curl_setopt_array($ch, $options);
+
+        $response = curl_exec($ch);
+        $response = json_decode($response, true);
+        $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        if((int)($statusCode / 100) !== 2) {
+            $error = curl_error($ch);
+            curl_close($ch);
+            error_log($error);
+            $base->reroute("/error");
+        }
+
+        curl_close($ch);
+        $base->reroute("/");
+    }
+
+    public function getEdit(\Base $base) {
+		if($base->get("SESSION.token") !== null) {
+			$postID = $base->get("PARAMS.entryID");
+
+	        $data = file_get_contents($base->get("QS.ATHEJA_SERVER_URL") . "/api/search/entry/" . $postID);
+	        $result = json_decode($data, true);
+
+	        $tags = "";
+	        foreach($result["tags"] as $tag){
+	        	$tags = $tags . $tag["name"] . ";";
+	        }
+	        
+	        $base->set("entry", $result);
+	        $base->set("tags", $tags);
+	        $base->set("content", "edit_entry.html");
+	        echo \Template::instance()->render("index.html");
+        } else {
+        	$base->reroute("/error");
+        }
+    }
+
+    public function postEdit(\Base $base) {
+		if($base->get("SESSION.token") === null) {
+        	$base->reroute("/error");
+        }
+
+        $ch = curl_init();
+
+        $postFields = [
+	        "page-name" => $base->get("POST.page-name"),
+	        "page-desc" => $base->get("POST.page-desc"),
+        ];
+
+        $options = [
+            CURLOPT_URL => $base->get("QS.ATHEJA_SERVER_URL") . "/api/search/entry/" . $base->get("PARAMS.entryID") . "/edit",
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_POST => true,
             CURLOPT_POSTFIELDS => http_build_query($postFields),
